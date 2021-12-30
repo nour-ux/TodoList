7b2a0be9a1aa26ba4b3c320be54a1a9809eb519a
nour-ux <64440592+nour-ux@users.noreply.github.com>
2021-12-30 03:50:14 +0300

    finish get/post

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const app=express()
 const db=require('./db')
 const Todo=require('./modeTodo')
 
-
+app.use(express.json())
 app.get('/',(req,res)=>{
 
     res.json('Get / is working ❤️✨')
@@ -12,10 +12,33 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/tasks',(req,res)=>{
+    Todo.find({},(err,data)=>{
+if(err){
 
-    res.json('Get / is working ❤️✨')
-    
+    console.log("Error:",err)
+}
+else{
+
+    res.json(data)
+} })    
 })
+
+
+app.post('/tasks',(req,res)=>{
+    console.log('25:',req.body)
+    Todo.create(req.body,(err,data)=>{
+if(err){
+
+    console.log("Error:",err)
+}
+else{
+
+    res.json(data);
+} })    
+})
+
+
+
 
 app.listen(5000,()=>{
 
